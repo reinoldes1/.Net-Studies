61d00212a6a6560a7efa10bf01900c52a5a4fe63
reinoldes <reinoldesxbox@gmail.com>
2025-01-31 22:16:49 -0300

    Add project files.

---
+++ TestaOsNomes/Program.cs
@@ -0,0 +1,24 @@
+﻿string firstName;
+string secondName;
+
+void TestaOsNomes()
+{
+    if (firstName == secondName)
+    {
+        Console.WriteLine("Os nomes são iguais");
+        Console.WriteLine($"Ambos os nomes são {firstName}");
+    }
+    else
+    {
+        Console.WriteLine("Os nomes são diferentes");
+        Console.WriteLine($"O primeiro nome é {firstName} e o segundo é {secondName}");
+    }
+}
+
+firstName = "Roberrrrrto";
+secondName = "Carlos";
+TestaOsNomes();
+
+firstName = "Robervaldo";
+secondName = "Robervaldo";
+TestaOsNomes();
