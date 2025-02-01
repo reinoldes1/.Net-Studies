d253bfdb4c4b6dc04a0d5a5f8718912c376e6269
reinoldes <reinoldesxbox@gmail.com>
2025-01-31 23:04:53 -0300

    Adicionando as funções InputUsuario e DesejaContinuar

--- TestaOsNomes/Program.cs
+++ TestaOsNomes/Program.cs
@@ -1,24 +1,66 @@
-﻿string firstName;
-string secondName;
-
-void TestaOsNomes()
+﻿class Program
 {
-    if (firstName == secondName)
+    static string firstName;
+    static string secondName;
+
+
+
+    static void TestaOsNomes()
     {
-        Console.WriteLine("Os nomes são iguais");
-        Console.WriteLine($"Ambos os nomes são {firstName}");
+        if (firstName == secondName)
+        {
+            Console.WriteLine("Os nomes são iguais");
+            Console.WriteLine($"Ambos os nomes são {firstName}\n");
+        }
+        else
+        {
+            Console.WriteLine("Os nomes são diferentes");
+            Console.WriteLine($"O primeiro nome é {firstName} e o segundo é {secondName}\n");
+        }
     }
-    else
+
+    static void InputUsuario()
     {
-        Console.WriteLine("Os nomes são diferentes");
-        Console.WriteLine($"O primeiro nome é {firstName} e o segundo é {secondName}");
+        Console.Write("Escolha um nome: ");
+        firstName = Console.ReadLine();
+        Console.Write("Escolha o nome que deseja comparar: ");
+        secondName = Console.ReadLine();
+    }
+
+    static bool DesejaContinuar()
+    {
+        Console.Write("Deseja Continuar (y/n): ");
+        string respostaUsuario = Console.ReadLine();
+
+        if (respostaUsuario == "y")
+        {
+            Console.WriteLine("Continuando o programa...\n");
+            Thread.Sleep(1000);
+            return true;
+        }
+
+        else if (respostaUsuario == "n")
+        {
+            Console.WriteLine("Fechando o programa...\n");
+            Thread.Sleep(2000);
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Opção invalida\n");
+            Thread.Sleep(2000);
+            return DesejaContinuar();
+        }
+    }
+
+    static void Main()
+    {
+        bool continuar = true;
+        while (continuar)
+        {
+            InputUsuario();
+            TestaOsNomes();
+            continuar = DesejaContinuar();
+        }
     }
 }
-
-firstName = "Roberrrrrto";
-secondName = "Carlos";
-TestaOsNomes();
-
-firstName = "Robervaldo";
-secondName = "Robervaldo";
-TestaOsNomes();
